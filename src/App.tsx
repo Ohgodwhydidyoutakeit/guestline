e1633680e457fc11b2906c058293dcedd4bef971
Ohgodwhydidyoutakeitm <w.lipowski97@gmail.com>
2022-02-27 23:34:23 +0100

    thunk added with redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import './App.css';
+import { fetchUserById } from './redux/apiSlice';
 
 function App() {
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(fetchUserById())
+  }, [])
   return (
     <div className="App">
-   
+
     </div>
   );
 }
